c138d6a08d1aa44e7f9c98708e9c743658e6e5e2
Nikita Sandhu <sandhunikita0096@gmail.com>
2020-03-06 02:29:49 -0500

    login validation

---
+++ C0766552_MidTerm_MAD3115W2020/Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  Extensions.swift
+//  C0766552_MidTerm_MAD3115W2020
+//
+//  Created by Nikita Sandhu on 2020-03-06.
+//  Copyright © 2020 Nikita Sandhu. All rights reserved.
+//
+
+import Foundation
+extension String{
+    func emailValid() -> Bool{      //https://stackoverflow.com/questions/27998409/email-phone-validation-in-swift
+        let emailRegEx = "[A-Z0-9a-z.%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: self)
+    }
+    
+    func mobileValid() -> Bool{         //https://stackoverflow.com/questions/27998409/email-phone-validation-in-swift
+        let mobileNumberRegEx = "[0-9]{10}"
+        let mobileTest = NSPredicate(format: "SELF MATCHES %@", mobileNumberRegEx)
+        return mobileTest.evaluate(with: self)
+    }
+}
--- C0766552_MidTerm_MAD3115W2020/SecondViewController.swift
+++ C0766552_MidTerm_MAD3115W2020/SecondViewController.swift
@@ -23,19 +23,29 @@ class SecondViewController: UIViewController {
         if sender.isOn{
             UserDefaults.standard.set(txtEmail.text, forKey: "email")
             UserDefaults.standard.set(txtPassword.text, forKey: "password")
+            
+            let ud = UserDefaults.standard
+            let email = ud.string(forKey: "email")
+            txtEmail.text = email
+            let password = ud.string(forKey: "password")
+            txtPassword.text = password
+            
         }else{
             UserDefaults.standard.removeObject(forKey: "email")
             UserDefaults.standard.removeObject(forKey: "password")
+            txtEmail.text = ""
+            txtPassword.text = ""
         }
     }
     
     @IBAction func bbLogin(_ sender: UIBarButtonItem) {
-//        if (txtEmail.text != nil && txtPassword.text != nil){
+        if (txtEmail.text?.emailValid() == true && txtPassword.text != ""){
             let sb = UIStoryboard(name: "Main", bundle: nil)
             let thirdVC = sb.instantiateViewController(identifier: "thirdVC") as! ThirdViewController
             self.navigationController?.pushViewController(thirdVC, animated: true)
-//        }else{
-//            print("Enter email and password")
-//        }
+        }else{
+            print("Enter valid email and password")
+        }
+            
     }
 }
