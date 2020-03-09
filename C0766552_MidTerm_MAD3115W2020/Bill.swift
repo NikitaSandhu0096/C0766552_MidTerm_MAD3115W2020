e6eb1f4a8c2be1a9db5381ab96ca951f072da6eb
Nikita Sandhu <sandhunikita0096@gmail.com>
2020-03-09 17:25:55 -0400

    bill class created

---
+++ C0766552_MidTerm_MAD3115W2020/Bill.swift
@@ -0,0 +1,29 @@
+//
+//  Bill.swift
+//  C0766552_MidTerm_MAD3115W2020
+//
+//  Created by MacStudent on 2020-03-09.
+//  Copyright © 2020 Nikita Sandhu. All rights reserved.
+//
+
+import Foundation
+
+enum type {
+    case Internet
+    case Mobile
+    case Hydro
+}
+
+class Bill {
+    
+    var billId : String
+    var billDate : String
+    var billType : type
+    var totalBillAmount : Float = 0.0
+    
+    init(billId : String, billDate : String, billType : type) {
+        self.billId = billId
+        self.billDate = billDate
+        self.billType = billType
+    }
+}
