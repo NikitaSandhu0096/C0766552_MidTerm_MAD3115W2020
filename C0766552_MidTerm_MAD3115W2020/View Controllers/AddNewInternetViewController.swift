1a5ae82b6964eaab938a2f5bbd737d76180693ef
Nikita Sandhu <sandhunikita0096@gmail.com>
2020-03-09 20:29:53 -0400

    hydro internet mobile VC

---
+++ C0766552_MidTerm_MAD3115W2020/View Controllers/AddNewInternetViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddNewInternetViewController.swift
+//  C0766552_MidTerm_MAD3115W2020
+//
+//  Created by MacStudent on 2020-03-09.
+//  Copyright © 2020 Nikita Sandhu. All rights reserved.
+//
+
+import UIKit
+
+class AddNewInternetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
