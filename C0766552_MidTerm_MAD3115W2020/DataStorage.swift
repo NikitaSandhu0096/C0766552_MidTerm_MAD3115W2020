e52fec95a03c79460d8883fccbe06186d878a70d
Nikita Sandhu <sandhunikita0096@gmail.com>
2020-03-13 18:30:43 -0400

    final

--- C0766552_MidTerm_MAD3115W2020/DataStorage.swift
+++ C0766552_MidTerm_MAD3115W2020/DataStorage.swift
@@ -11,7 +11,6 @@ import Foundation
 class DataStorage{
     private static let instance = DataStorage()
     private lazy var customerDictionary = [String : Customer]()
-//    private lazy var billDictionary = [String : Bill]()
     
     private init(){}
     
@@ -24,10 +23,6 @@ class DataStorage{
         self.customerDictionary.updateValue(customer, forKey: cid)
     }
     
-//    func addBillToCustomer(bill : Bill){
-//        let bid = bill.billId
-//        self.billDictionary.updateValue(bill, forKey: bid)
-//    }
     
     func loadCustomerDetails(){
 //        customerDictionary.updateValue(Customer(customerId: "C0001", firstName: "Nikita", lastName: "Sandhu", email: "nsandhu@gmail.com"), forKey: "C0001")
@@ -79,8 +74,5 @@ class DataStorage{
 //        return Array(customerDictionary.values)
     }
 //    
-//    func getAllBills() -> [Bill]{
-//        return Array(self.billDictionary.values)
-//    }
     
 }
